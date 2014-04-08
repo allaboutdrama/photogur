8079e8eed2bda04c85c0ab68bc258e0f16130f39
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-08 16:43:26 -0400

    Using static routes to make a pic show at its own URL

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,13 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = [
+      {
+        :title   => "An Old Church",
+        :artist  => "Sergei Ershov",
+        :url     => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      }
+    ]
   end
-
 end
+
+

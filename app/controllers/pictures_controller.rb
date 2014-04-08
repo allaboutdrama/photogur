d3025363f924b8a4d46a3dde8197023eb947143a
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-08 19:35:23 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,39 +1,14 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title   => "An Old Church",
-        :artist  => "Sergei Ershov",
-        :url     => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-
-      ]
+    @pictures = Picture.all
   end
 
-  def show
-    @pictures = [
-      {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-      },
-      {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      },
-   ]
-  @picture = @pictures[params[:id].to_i]
+  def show    
+    @picture = Picture.find(params[:id])
   end
 
   def new
   end
-
 
   def create
     render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]} "

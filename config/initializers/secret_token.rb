19a61b0705ab10c6bfebdfa83f664647d99b238a
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-08 14:17:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = 'd4f364016c94fb92b907a9c2e13bbd5771165a496bfced9430885748448fc15345324d46027d5c4da51a9bb7d88fa94707257de0bf5c9e3185b677dd1a59e502'

221aaf598b4e88e2fc69873f17efe805c6a7c401
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-08 23:29:25 -0400

    Added feature to delete image

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Photogur::Application.routes.draw do
 
 
 Photogur::Application.routes.draw do
-  root :to => "pictures#index"
+  
   get 'pictures' => 'pictures#index'
 
   post 'pictures'     => 'pictures#create'
@@ -66,6 +66,10 @@ Photogur::Application.routes.draw do
 
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
+
+  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
+
+  root :to => "pictures#index"
 
 end  
 
